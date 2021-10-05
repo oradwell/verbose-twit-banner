2818312c09d9d1874741db6e5e06c1ac6f855d07
Oliver Radwell <oliver.radwell@gmail.com>
2021-10-05 23:02:40 +0100

    Some tidy

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "bufio"
 import "github.com/golang/freetype"
+import "github.com/golang/freetype/truetype"
 import "image"
 import "image/color"
 import "image/draw"
@@ -10,10 +11,8 @@ import "io/ioutil"
 import "os"
 import _ "image/jpeg"
 
-func main() {
-	const fontFileName = "fonts/OpenSans-VariableFont_wdth,wght.ttf"
-
-	file, err := os.Open("images/skate-1500x500.jpg")
+func getDrawableFromImagePath(imagePath string) *image.RGBA {
+	file, err := os.Open(imagePath)
 	if err != nil {
 		panic(err)
 	}
@@ -26,18 +25,14 @@ func main() {
 	}
 
 	decodedImageBounds := decodedImage.Bounds()
-	decodedRectangle := image.NewRGBA(image.Rect(0, 0, decodedImageBounds.Dx(), decodedImageBounds.Dy()))
-	draw.Draw(decodedRectangle, decodedRectangle.Bounds(), decodedImage, decodedImageBounds.Min, draw.Src)
+	drawable := image.NewRGBA(image.Rect(0, 0, decodedImageBounds.Dx(), decodedImageBounds.Dy()))
+	draw.Draw(drawable, drawable.Bounds(), decodedImage, decodedImageBounds.Min, draw.Src)
 
-	greyRectangle := image.Rect(1082, 22, 1482, 272)
-	greyBg := image.NewRGBA(greyRectangle)
-	grey := color.RGBA{150, 150, 150, 255}
-	opacity := color.Alpha{150}
-	draw.DrawMask(decodedRectangle, greyBg.Bounds(), &image.Uniform{grey}, image.ZP, &image.Uniform{opacity}, image.ZP, draw.Over)
+	return drawable
+}
 
-	ftContext := freetype.NewContext()
-
-	fontBytes, err := ioutil.ReadFile(fontFileName)
+func getFontFromFontPath(fontPath string) *truetype.Font {
+	fontBytes, err := ioutil.ReadFile(fontPath)
 	if err != nil {
 		panic(err)
 	}
@@ -47,32 +42,80 @@ func main() {
 		panic(err)
 	}
 
-	fontSize := 32
-	ftContext.SetDPI(72)
-	ftContext.SetFont(font)
-	ftContext.SetFontSize(float64(fontSize))
-	ftContext.SetClip(decodedRectangle.Bounds())
-	ftContext.SetDst(decodedRectangle)
-	ftContext.SetSrc(image.Black)
+	return font
+}
 
-	labelPoint := freetype.Pt(greyRectangle.Min.X+10, greyRectangle.Min.Y+fontSize)
+func addOverlay(drawable *image.RGBA, rectangle image.Rectangle, colour *color.RGBA, opacity *color.Alpha) {
+	draw.DrawMask(drawable, rectangle, &image.Uniform{colour}, image.ZP, &image.Uniform{opacity}, image.ZP, draw.Over)
+}
 
-	_, err = ftContext.DrawString("204 Following", labelPoint)
+func getFreetypeContext(font *truetype.Font, dpi float64, fontSize float64, drawable *image.RGBA) *freetype.Context {
+	context := freetype.NewContext()
+	context.SetDPI(dpi)
+	context.SetFont(font)
+	context.SetFontSize(fontSize)
+	context.SetClip(drawable.Bounds())
+	context.SetDst(drawable)
+	context.SetSrc(image.Black)
+
+	return context
+}
+
+func addLines(rectangle image.Rectangle, context *freetype.Context, lines []string, fontSize int, padding int) {
+	pointX := rectangle.Min.X + padding
+	pointY := rectangle.Min.Y + fontSize
+
+	for _, text := range lines {
+		labelPoint := freetype.Pt(pointX, pointY)
+		_, err := context.DrawString(text, labelPoint)
+		if err != nil {
+			panic(err)
+		}
+
+		pointY += fontSize + padding
+	}
+}
+
+func writeToPng(filename string, drawable *image.RGBA) {
+	outFile, err := os.Create(filename)
 	if err != nil {
 		panic(err)
 	}
 
-	labelPoint = freetype.Pt(greyRectangle.Min.X+10, greyRectangle.Min.Y+fontSize*2+10)
+	png.Encode(outFile, drawable)
+}
 
-	_, err = ftContext.DrawString("68 Followers", labelPoint)
-	if err != nil {
-		panic(err)
-	}
+func main() {
+	const fontPath = "fonts/OpenSans-VariableFont_wdth,wght.ttf"
+	const outPath = "images/out.png"
+	const srcPath = "images/skate-1500x500.jpg"
+	const fontDpi = 72.0
+	const fontSize = 32.0
+	const overlayColourRed = 150
+	const overlayColourGreen = 150
+	const overlayColourBlue = 150
+	const overlayOpacity = 150
+	const overlayX0 = 1082
+	const overlayY0 = 22
+	const overlayX1 = 1482
+	const overlayY1 = 272
+	const textPadding = 10
 
-	outFile, err := os.Create("images/out.png")
-	if err != nil {
-		panic(err)
-	}
+	lines := []string{"204 Following", "68 Followers", "405 Tweets"}
 
-	png.Encode(outFile, decodedRectangle)
+	overlayColour := color.RGBA{overlayColourRed, overlayColourBlue, overlayColourGreen, 255}
+
+	drawable := getDrawableFromImagePath(srcPath)
+
+	overlayRectangle := image.Rect(overlayX0, overlayY0, overlayX1, overlayY1)
+
+	addOverlay(drawable, overlayRectangle, &overlayColour, &color.Alpha{overlayOpacity})
+
+	font := getFontFromFontPath(fontPath)
+
+	ftContext := getFreetypeContext(font, fontDpi, fontSize, drawable)
+
+	addLines(overlayRectangle, ftContext, lines, int(fontSize), textPadding)
+
+	writeToPng(outPath, drawable)
 }
